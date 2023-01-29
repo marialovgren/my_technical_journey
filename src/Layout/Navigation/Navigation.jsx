faaa8a54214b40679ac903f8ef87b59605a85835
marialovgren <marialovgren86@hotmail.com>
2023-01-29 14:13:00 +0100

    adjusted hamburgermenu to close on click on link in navbar

--- src/Layout/Navigation/Navigation.jsx
+++ src/Layout/Navigation/Navigation.jsx
@@ -14,54 +14,63 @@ const Navigation = () => {
     }
 
     return (  
-        <nav className="w-full bg-lightBeige shadow-md shadow-lightBeige">
-            <div className="justify-between px-3 mx-2 md:items-center md:flex md:px-8"> 
-                <div>
-                    <div className="flex items-center justify-between py-2 md:py-4 md:block">
-                        <div>
-                            <img src={logo.img} className="h-14" />
-                        </div>
+        <div className="relative"> 
+            <nav className="top-0 left-0 z-20 w-full bg-lightBeige shadow-md shadow-lightBeige">
+                <div className="justify-between px-3 mx-2 md:items-center md:flex md:px-8"> 
+                    <div>
+                        <div className="flex items-center justify-between py-2 md:py-4 md:block">
+                            <div>
+                                <img src={logo.img} className="h-14" />
+                            </div>
 
-                        <div className="md:hidden ">
-                            <button
-                                className="text-3xl absolute right-6 top-4 cursor-pointer"
-                                onClick={() => setOpen(!open)}
-                            >
-                                {open ? (
-                                     <ion-icon name="close-outline"></ion-icon>
-                                ) : (
-                                    <ion-icon name="menu-outline"></ion-icon>
-                                )}
-                            </button>
+                            <div className="md:hidden ">
+                                <button
+                                    className="text-3xl absolute right-6 top-4 cursor-pointer"
+                                    onClick={() => setOpen(!open)}
+                                >
+                                    {open ? (
+                                        <ion-icon name="close-outline"></ion-icon>
+                                    ) : (
+                                        <ion-icon name="menu-outline"></ion-icon>
+                                    )}
+                                </button>
+                            </div>
                         </div>
-                    </div>
-                </div>  
-                <div>
-                    <div
-                        className={`flex-1 justify-self-center pb-3 mt-8 md:block md:pb-0 md:mt-0 ${
-                            open ? "block" : "hidden"
-                        }`}
-                    >
-                        <ul className="flex flex-col md:flex-row md:items-center md:pb-0 pb-3 md:static md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0 pl-9 transition-all duration-500 ease-in md:space-x-14 items-center text-blue">
-                            {navLinks.map((navLink) => {
-                            const {id, navLinkTitle, navLinkUrl} = navLink
-                                return (
-                                    <NavigationLink 
-                                        key={id}
-                                        link={navLinkUrl}
-                                        onClick={ () => {
-                                            setOpen(false)
-                                        }}
-                                    >
-                                        {navLinkTitle}
-                                    </NavigationLink>
-                                )
-                            })}
-                        </ul> 
-                    </div>
+                    </div>  
+                    <div>
+                        <div
+                            className={`flex-1 justify-self-center pb-3 mt-8 md:block md:pb-0 md:mt-0 ${
+                                open ? "block" : "hidden"
+                            }`}
+                        >
+                            <ul className="flex flex-col md:flex-row md:items-center md:pb-0 pb-3 md:static md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0 pl-9 transition-all duration-500 ease-in md:space-x-14 items-center text-blue">
+                                {navLinks.map((navLink) => {
+                                const {id, navLinkTitle, navLinkUrl} = navLink
+                                    return (
+                                        <NavigationLink 
+                                            key={id}
+                                            link={navLinkUrl}
+                                            setOpen={setOpen}
+                                        >
+                                            {navLinkTitle}
+                                        </NavigationLink>
+                                    )
+                                })}
+                            </ul> 
+                        </div>
+                    </div> 
                 </div> 
-            </div> 
-        </nav>
+            </nav>
+            <div 
+                onClick={() => setOpen(false)}
+                className={open 
+                    ? "absolute top-385 left-0 h-full w-full -z-5"
+                    : ""
+                }
+            >
+
+            </div>
+        </div>
     )
 }
 
--- src/Layout/Navigation/NavigationLink/NavigationLink.jsx
+++ src/Layout/Navigation/NavigationLink/NavigationLink.jsx
@@ -1,14 +1,15 @@
 import { NavLink } from 'react-router-dom'
 
-const NavigationLink = ({children, link}) => {
+const NavigationLink = ({children, link, setOpen}) => {
     const activeLink = 'underline decoration-2 decoration-orange underline-offset-8 '
     const defaultLink = ''
 
     return (
         <li 
-            className="md;ml-8 md:my-0 my-4 font-medium cursor-pointer hover:font-bold underline-offset-8 list-none"
+            className="md:ml-8 md:my-0 my-4 font-medium cursor-pointer hover:font-bold underline-offset-8 list-none"
         >
             <NavLink to={link}
+            onClick={() => setOpen(false)}
                 className={ ({ isActive }) => isActive ? activeLink : defaultLink }
             >
                 {children}
