1db55ba83ba7c5b93f7267a9272e5fd447a15837
marialovgren <marialovgren86@hotmail.com>
2023-02-04 14:11:40 +0100

    adjusted style for NavLinks

--- src/Layout/Navigation/NavigationLink/NavigationLink.jsx
+++ src/Layout/Navigation/NavigationLink/NavigationLink.jsx
@@ -1,11 +1,9 @@
 
 const NavigationLink = ({children, link, setOpen}) => {
-    const activeLink = 'underline decoration-2 decoration-orange underline-offset-8 '
-    const defaultLink = ''
 
     return (
         <li 
-            className="md:ml-8 md:my-0 my-4 font-medium cursor-pointer hover:font-bold underline-offset-8 list-none"
+            className="md:ml-8 md:my-0 my-4 font-medium cursor-pointer hover:font-bold underline-offset-8 list-none hover:underline hover:decoration-2 hover:decoration-orange hover:underline-offset-8"
         >
             <a href={link}
                 onClick={() => setOpen(false)}
