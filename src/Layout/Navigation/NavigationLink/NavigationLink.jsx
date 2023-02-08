6f751c314f39d7138312994b16655b289431c3c7
marialovgren <marialovgren86@hotmail.com>
2023-02-08 20:43:48 +0100

    another atempt to make navlinks work on Netlify

--- src/Layout/Navigation/NavigationLink/NavigationLink.jsx
+++ src/Layout/Navigation/NavigationLink/NavigationLink.jsx
@@ -2,9 +2,9 @@
 const NavigationLink = ({children, link, setOpen}) => {
 
     return (
-        <a href={link}
-                onClick={() => setOpen(false)}
-            >
+        <a href={(link)}
+            onClick={() => setOpen(false)}
+        >
             <li 
                 className="md:ml-8 md:my-0 my-4 font-medium cursor-pointer hover:font-bold underline-offset-8 list-none hover:underline hover:decoration-2 hover:decoration-orange hover:underline-offset-8"
             >
