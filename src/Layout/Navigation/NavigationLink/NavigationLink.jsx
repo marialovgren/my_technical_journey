392f73f388038a7318dc08e64e511a4a5d84cff9
marialovgren <marialovgren86@hotmail.com>
2023-02-08 20:37:22 +0100

    issue accessing navlink on netlify. Trying to solve

--- src/Layout/Navigation/NavigationLink/NavigationLink.jsx
+++ src/Layout/Navigation/NavigationLink/NavigationLink.jsx
@@ -2,15 +2,15 @@
 const NavigationLink = ({children, link, setOpen}) => {
 
     return (
-        <li 
-            className="md:ml-8 md:my-0 my-4 font-medium cursor-pointer hover:font-bold underline-offset-8 list-none hover:underline hover:decoration-2 hover:decoration-orange hover:underline-offset-8"
-        >
-            <a href={link}
+        <a href={link}
                 onClick={() => setOpen(false)}
             >
+            <li 
+                className="md:ml-8 md:my-0 my-4 font-medium cursor-pointer hover:font-bold underline-offset-8 list-none hover:underline hover:decoration-2 hover:decoration-orange hover:underline-offset-8"
+            >
                 {children}
-            </a>
-        </li>
+            </li>
+        </a>
     )
 }
 
